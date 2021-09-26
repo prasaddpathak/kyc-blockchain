cdd2f8da8d5dc2c072902c33cfc363aff6b612d3
Prasad Pathak <prasaddpathak@gmail.com>
2021-09-26 23:42:47 +0530

    dry run and bug fixes

--- contracts/kyc.sol
+++ contracts/kyc.sol
@@ -37,7 +37,7 @@ contract KYC{
     
     mapping(address => string[]) customerVotes;
     
-    mapping(address => string[999]) bankComplaints;
+    mapping(address => string[]) bankComplaints;
 
     constructor() public {
         bankCount = 0;
@@ -53,11 +53,14 @@ contract KYC{
         customers[_userName].bank = msg.sender;
     }
     
-    function viewCustomer(string memory _userName) isBank() public view returns (string memory, string memory, address)  {
+    function viewCustomer(string memory _userName) isBank() public view returns (string memory, string memory, address, bool, uint, uint)  {
         require(customers[_userName].bank != address(0), "Customer is not present in the database");
         return (customers[_userName].userName, 
                 customers[_userName].customerData, 
-                customers[_userName].bank);
+                customers[_userName].bank,
+                customers[_userName].kycStatus,
+                customers[_userName].upVotes,
+                customers[_userName].downVotes);
     }
     
     function modifyCustomer(string memory _userName, string memory _newcustomerData) isBank() public {
@@ -69,6 +72,7 @@ contract KYC{
     }
 
     function addRequest(string memory _userName, string memory _customerData) isBank() public{
+        require(customers[_userName].bank != address(0), "Customer is not present in the database");
         require(kycRequestList[_userName].bankAddress == address(0), "KYC Request for this customer is already present!");
         kycRequestList[_userName].userName = _userName;
         kycRequestList[_userName].customerData = _customerData;
@@ -79,8 +83,9 @@ contract KYC{
 
     function removeRequest(string memory _userName) isBank() public{
         require(kycRequestList[_userName].bankAddress != address(0), "KYC Request for this customer is not present!");
+        banks[kycRequestList[_userName].bankAddress].kycCount -= 1;
         delete kycRequestList[_userName];
-        banks[kycRequestList[_userName].bankAddress].kycCount -=1;
+        
     }
       
     function upvoteCustomer(string memory _userName) isBank() hasAlreadyVoted(_userName) isAllowedToVote() public{
@@ -90,7 +95,8 @@ contract KYC{
     }  
 
     function downvoteCustomer(string memory _userName) isBank() hasAlreadyVoted(_userName) isAllowedToVote() public{
-        customers[_userName].downVotes += 1;
+        customerVotes[msg.sender].push(customers[_userName].userName);
+        customers[_userName].downVotes -= 1;
         changeKycStatus(_userName);
     }  
     
@@ -107,7 +113,8 @@ contract KYC{
         return banks[_bankAddress].complaintsRecoreded;
     }  
 
-    function viewBankDetails(address _bankAddress) public view returns (string memory, address, string memory, uint, uint, bool) {
+    function viewBankDetails(address _bankAddress) isBank() public view returns (string memory, address, string memory, uint, uint, bool) {
+        require(banks[_bankAddress].ethAddress != address(0), "Invalid address! Bank does not exist. ");
         return (banks[_bankAddress].name,
                 banks[_bankAddress].ethAddress,
                 banks[_bankAddress].regNumber,
@@ -116,8 +123,8 @@ contract KYC{
                 banks[_bankAddress].isAllowedToVote);
     }  
 
-    function reportBank(string memory _bankName, address _bankAddress) isBank() public{
-        //Check if already reported??
+    function reportBank(string memory _bankName, address _bankAddress) isBank() hasAlreadyReportedBank(_bankName) public{
+        bankComplaints[msg.sender].push(_bankName);
         banks[_bankAddress].complaintsRecoreded += 1; 
         if (banks[_bankAddress].complaintsRecoreded > (bankCount/3)) {
             banks[_bankAddress].isAllowedToVote = false;
@@ -127,7 +134,7 @@ contract KYC{
     //Admin Functions
 
     function addBank(string memory _bankName, address _bankAddress, string memory _regNumber) isAdmin() public{
-        require(banks[_bankAddress].ethAddress == address(0), "Bank with this Name is already present");
+        require(banks[_bankAddress].ethAddress == address(0), "Bank with this address is already present");
         banks[_bankAddress].name = _bankName;
         banks[_bankAddress].ethAddress = _bankAddress;
         banks[_bankAddress].regNumber = _regNumber;
@@ -170,6 +177,16 @@ contract KYC{
 
     modifier isAllowedToVote {
         require(banks[msg.sender].isAllowedToVote == true, "Your bank is not allowed to vote!");
+        _;
+    }
+    
+    modifier hasAlreadyReportedBank (string memory _bankName) {
+        string[] memory complaints = bankComplaints[msg.sender];
+        for (uint i=0; i < complaints.length; i++){
+            if (keccak256(bytes(complaints[i])) == keccak256(bytes(_bankName))){
+               revert("You have already reported this bank"); 
+            }
+        }
         _;
     }
 
